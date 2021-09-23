ebcbccd435c1111121cb859c3f41ae358ebde3ab
Wesley <wmixgamer@gmail.com>
2021-09-23 01:12:23 -0300

    Documentação Swagger adicionada ao projeto

--- Back-End/senai_hroads_webAPI/senai_hroads_webAPI/Startup.cs
+++ Back-End/senai_hroads_webAPI/senai_hroads_webAPI/Startup.cs
@@ -3,9 +3,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace senai_hroads_webAPI
@@ -18,11 +21,35 @@ namespace senai_hroads_webAPI
         {
             services
                 .AddControllers();
+            services.AddSwaggerGen();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API Hroads",
+                    Description = "Documentação Swagger da API do Projeto Hroads"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
